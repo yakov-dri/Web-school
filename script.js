5776afab86d548bba0b7e44d90809c458658815c
yakov <yakov@gmail.com>
2021-12-22 23:43:19 +0200

    Added files index.html and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let num1 = prompt("Enter number 1");
+let num2 = prompt("Enter number 2");
+
+let divPrompt = parseInt(num1 / num2);
+let sum = Math.round(divPrompt);
+
+alert("The result of the division between num1 and num2 is result: " + sum);
